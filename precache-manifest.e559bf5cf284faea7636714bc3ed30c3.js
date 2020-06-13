7e1ce13bd34e4f098b75e02d4cdc25364c01d486
abhi19shek <60101249+abhi19shek@users.noreply.github.com>
2020-06-14 00:14:08 +0530

    Updates

---
+++ precache-manifest.e559bf5cf284faea7636714bc3ed30c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dddd5688296eb9193c108d0a6deb9774",
+    "url": "/findmind/index.html"
+  },
+  {
+    "revision": "2924308a9ff390bc0adc",
+    "url": "/findmind/static/css/main.a7fe0171.chunk.css"
+  },
+  {
+    "revision": "f3e2e090467f7ffd05ae",
+    "url": "/findmind/static/js/2.9ae0c50f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/findmind/static/js/2.9ae0c50f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2924308a9ff390bc0adc",
+    "url": "/findmind/static/js/main.c97974d4.chunk.js"
+  },
+  {
+    "revision": "cddbaf86eed121385756",
+    "url": "/findmind/static/js/runtime-main.af0dee59.js"
+  }
+]);
